809a67a0901c9e112939c4119eecb72829eeee9f
Mistermasque <mistermasque@gmail.com>
2024-02-05 21:47:47 +0100

    Ajout barre chargement

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,18 @@
-import { ReactNode } from "react"
+import { LinearProgress } from "@mui/material"
 
 export default function Loading() {
   // Or a custom loading skeleton component
-  return <div>Chargement...</div>
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 3000,
+        width: "100%",
+      }}
+    >
+      <LinearProgress />
+    </div>
+  )
 }
